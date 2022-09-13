2c031e1ca76723b0ea1b86bff9f90bf24821d56c
CodewithAmann <113423671+CodewithAmann@users.noreply.github.com>
2022-09-14 00:31:48 +0530

    Update main.c

--- aman/main.c
+++ aman/main.c
@@ -1,1 +1,13 @@
-sdlkj tk l;gk fg
+#include<stdio.h>
+int main ()
+{
+    int a;
+    printf("ENTER NUMBER YOU WANT TO FIND ITS TABLE FORM \n");
+    scanf("%d",&a);
+
+    for (int  i = 0; i <=10; i++)
+    {
+        printf("%d multiply %d = %d \n",a,i,a*i);
+    }
+    return 0;
+}
